0070eaab620d3064683e5bc639f642d815fa81fc
catakirem <altunirem2000@gmail.com>
2025-08-15 15:47:18 +0300

    Add files via upload

---
+++ odev 3.sql
@@ -0,0 +1,7 @@
+-- Odev 3
+-- select country from country where country like 'A%a'
+-- select country from country where country like 'A%a'
+-- select country from country where length(country)>=6 and country like '%n'
+-- SELECT title FROM film WHERE ( LENGTH(LOWER(title)) - LENGTH(REPLACE(LOWER(title), 't', ''))
+) >= 4;
+-- SELECT * FROM film WHERE title ILIKE 'C%' AND length > 90 AND rental_rate = 2.99;
